08c2514c069a1adcaaa61157ac10bf944cd2874c
Sabina-Salimova <salimovas@spu.edu>
2026-03-06 01:05:06 -0800

    Added caregiver dashboard with assigned residents, notifications, and API updates

--- src/screens/loginScreen.tsx
+++ src/screens/loginScreen.tsx
@@ -49,7 +49,7 @@ export default function Login({ navigation }: any) {
           navigation.replace("AdminDashboard");
           break;
         case "ROLE_CAREGIVER":
-          navigation.replace("Home");
+          navigation.replace("CaregiverDashboard");
           break;
         case "ROLE_KITCHEN":
           navigation.replace("Home");
